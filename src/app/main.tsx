9842a9c851774a7efa6e26b7aa1b06a1bb0f46c1
Carlos Garita Campos <138426063+cgaritac@users.noreply.github.com>
2025-11-04 18:48:34 -0600

    Feat: Add changes (#9)
    * Feat: Add tailwind configuration
    
    * Feat: Add important dependencies
    
    * Feat: Add components and assets
    
    * Feat: Add public assets
    
    ---------
    
    Co-authored-by: carlos.garita <carlos.garita@factork-software.com>

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/shared/styles/index.css'
-import App from './configuration/App.tsx'
+import { App } from './configuration/App.tsx'
 import { initFirebase } from './hoting/firebase.hosting.ts'
 
 initFirebase();

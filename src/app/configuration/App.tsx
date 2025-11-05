8978d47e9ad57230ec0f92503d6020d90343515f
carlos.garita <carlos.garita@factork-software.com>
2025-11-04 18:19:13 -0600

    Feat: Add important dependencies

--- src/app/configuration/App.tsx
+++ src/app/configuration/App.tsx
@@ -1,31 +1,9 @@
-import { useState } from 'react';
-import reactLogo from '@/shared/assets/react.svg';
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hola Mundo</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p className="text-3xl font-bold underline">Hola recontramundo</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App

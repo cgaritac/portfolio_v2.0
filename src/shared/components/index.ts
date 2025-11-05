a4a9c9bb01b64de4180dce3e711af949b30a8170
carlos.garita <carlos.garita@factork-software.com>
2025-11-04 22:36:52 -0600

    Feat: Add page sections

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,20 +1,6 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import About from './About';
-import Tech from './Tech';
-import Experience from './Experience';
-import Works from './Works';
-import Feedbacks from './Feedbacks';
-import Contact from './Contact';
-
 export {
-  About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
-  Contact,
   EarthCanvas, 
   BallCanvas, 
   ComputersCanvas, 
   StarsCanvas
-}
+} from './canvas';

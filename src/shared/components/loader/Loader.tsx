15d713bd18c16f61394d67e5752147f40fddaafd
carlos.garita <carlos.garita@factork-software.com>
2025-11-07 00:33:31 -0600

    Feat: Add loader component

--- src/shared/components/loader/Loader.tsx
+++ src/shared/components/loader/Loader.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
+import { Html, useProgress } from "@react-three/drei";
 
 export const Loader: FC = () => {
+  const { progress } = useProgress();
+
   return (
-    <div>Loader</div>
+    <Html>
+      <span className="canvas-loader"></span>
+      <p className="canvas-loader-text">{progress.toFixed(2)}%</p>
+    </Html>
   )
 }

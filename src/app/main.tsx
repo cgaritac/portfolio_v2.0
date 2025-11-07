dd2e1b2bf91fe1738ed87ef010f1e92ec04a116d
carlos.garita <carlos.garita@factork-software.com>
2025-11-07 01:10:22 -0600

    Fix: Rendering error

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,11 @@
 import '@/shared/styles/index.css'
+import 'react'
 import { StrictMode } from 'react'
+import 'react-dom'
 import { createRoot } from 'react-dom/client'
 import { App } from './configuration'
 import { initReactDevToolsFix } from './devtools'
 
-// fix for React DevTools with React 19
 initReactDevToolsFix()
 
 createRoot(document.getElementById('root')!).render(

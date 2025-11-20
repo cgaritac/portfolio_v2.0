71ea8c64239b6f10bf8db2e0dc8bb045444c7685
carlos.garita <carlos.garita@factork-software.com>
2025-11-20 00:57:42 -0600

    Feat: Add project images

--- src/widgets/works/assets/index.ts
+++ src/widgets/works/assets/index.ts
@@ -2,3 +2,7 @@ export { default as github } from "./github.png";
 export { default as project } from "./project.png";
 export { default as uiLibrary } from "./ui_library.png";
 export { default as expropriation } from "./expropriation.png";
+export { default as paceFE } from "./pace-fe.png";
+export { default as paceBE } from "./pace-be.png";
+export { default as helicopterGame } from "./helicopter.png";
+export { default as airplanePassengers } from "./airplane-passengers.png";

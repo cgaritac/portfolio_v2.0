7f29d10f04e57ccd8ed5b0abb912518a18366791
Carlos Garita Campos <cgaritac@gmail.com>
2025-11-29 13:20:32 -0600

    Feat: Change form colors

--- src/widgets/contact/ui/Contact.tsx
+++ src/widgets/contact/ui/Contact.tsx
@@ -8,7 +8,7 @@ const Contact: FC = () => {
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.article 
         variants={slideIn({ direction: "left", type: "tween", delay: 0.2, duration: 1 })} 
-        className="flex-[0.75] bg-black-100 rounded-2xl p-8"
+        className="flex-[0.75] bg-green-50/10 backdrop-blur-[2px] rounded-2xl p-8"
       >
         <header>
           <SectionTitle titleDescription="Get in touch" title="Contact." />

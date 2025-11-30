a208c8f0ebfdc7cb0d2e112bad231176a30d48bf
Carlos Garita Campos <cgaritac@gmail.com>
2025-11-30 00:46:58 -0600

    Fix: Eliminate import not used

--- src/shared/translations/i18n.ts
+++ src/shared/translations/i18n.ts
@@ -1,22 +1,6 @@
 import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import { en, es } from "./content";
-
-// i18n
-//   .use(LanguageDetector)
-//   .use(initReactI18next)
-//   .init({
-//     resources: {
-//       en: { translation: en },
-//       es: { translation: es }
-//     },
-//     fallbackLng: "en",
-//     interpolation: { escapeValue: false },
-//     detection: {
-//       order: ["navigator", "localStorage", "cookie"]
-//     }
-//   });
 
 const storedLang = typeof window !== 'undefined' ? localStorage.getItem('i18nextLng') : null;
 const initialLng = storedLang && (storedLang === 'en' || storedLang === 'es') ? storedLang : 'en';

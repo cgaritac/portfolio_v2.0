8c8a1b48048e35a4447aec1145a34ef9ce4ac0d9
Carlos Garita Campos <cgaritac@gmail.com>
2025-12-02 22:19:23 -0600

    Fix: Several errors

--- src/widgets/about/hooks/useServices.ts
+++ src/widgets/about/hooks/useServices.ts
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { backend, software, frontend, database } from "../assets";
 
 interface Service {
+  id: string;
   title: string;
   icon: string;
 }
@@ -11,9 +12,9 @@ export const useServices = (): Service[] => {
   const { t } = useTranslation();
 
   return useMemo(() => [
-    { title: t("about.services.frontend"), icon: frontend },
-    { title: t("about.services.backend"), icon: backend },
-    { title: t("about.services.software"), icon: software },
-    { title: t("about.services.database"), icon: database },
+    { id: "frontend", title: t("about.services.frontend"), icon: frontend },
+    { id: "backend", title: t("about.services.backend"), icon: backend },
+    { id: "software", title: t("about.services.software"), icon: software },
+    { id: "database", title: t("about.services.database"), icon: database },
   ], [t]);
 };
